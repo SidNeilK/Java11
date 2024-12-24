c57e4085364b198bcf258c8e2ecc004fab0b292b
SidNeilK <siddharthneilk@gmail.com>
2024-12-24 17:37:16 -0600

    init'ed

---
+++ Hello.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Hello {
+    public static void main(String[] args) {
+        //Scanner scanner = new Scanner(System.in);
+        //long y = scanner.nextLong();
+        //long x = 922337203922337203l;
+        //short y = 2000000000;
+        //int z = 1000000000;
+        //System.out.println(x);
+        //System.out.println(y);        
+        //System.out.println(z);
+
+        System.out.println(Short.MAX_VALUE+" "+ Short.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE+" "+Integer.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE+" "+Long.MIN_VALUE);
+
+
+    
+        }
+//Anything done by switch can also be done by if-else statments but is simply easier to write and understand
+//So only in more specific situations does the if-else statement is truly effective
+//else it is simply easier to use the switch statements
+            //32,767
+            //2,147,483,647
+            //9,223,372,036,854,775,807
+        
+
+    }
+
---
+++ net/JavaApplication1/src/Hello.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author siddh
+ */
+class Hello {
+    public static void main(String[] args) {
+
+        long x = 92233720368807;
+        short y = 2000;
+        int z = 1000000000;
+        System.out.println(x);
+        System.out.println(y);        
+        System.out.println(z);
+
+
+
+    
+        }
+//Anything done by switch can also be done by if-else statments but is simply easier to write and understand
+//So only in more specific situations does the if-else statement is truly effective
+//else it is simply easier to use the switch statements
+            
+        
+
+    }
