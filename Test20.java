841e5d1684048eecac6572d6bf362a1c740ae3fe
SidNeilK <siddharthneilk@gmail.com>
2024-12-25 18:21:55 -0600

    greater common divisor using Euclidean

---
+++ Test20.java
@@ -0,0 +1,12 @@
+public class Test20 {
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    
+    
+}
