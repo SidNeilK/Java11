4f6cb6550543675307e417196774595b20dc0768
SidNeilK <siddharthneilk@gmail.com>
2024-12-25 18:10:47 -0600

    char array

---
+++ Test9.java
@@ -0,0 +1,9 @@
+public class Test9 {
+    public static void main(String[] args) {
+        char[] title = {'T', 'i', 't', 'a', 'n', 'i', 'c'};
+        char ch = title[1];                
+        title[3] = ch;                       
+        System.out.println(title);           
+        System.out.println(ch);              
+    }
+}
