be1e2753c6008e46686108a475f5b55ff6050bf9
SidNeilK <siddharthneilk@gmail.com>
2024-12-25 18:12:46 -0600

    manipulating float

---
+++ Test12.java
@@ -0,0 +1,9 @@
+public class Test12 {
+    public static void main(String[] args) {
+        int n;
+        float x = 3.8f;
+        n = (int) x;
+        System.out.println("n = " + n);
+
+    }
+}
