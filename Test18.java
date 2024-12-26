32ee24551b30634b6f6bc3b2c9df50ed41fdd460
SidNeilK <siddharthneilk@gmail.com>
2024-12-25 18:20:01 -0600

    find a specific digit name

---
+++ Test18.java
@@ -0,0 +1,38 @@
+public class Test18 {
+        public static void digitName(int n) {
+            switch (n) {
+                case 1:
+                    System.out.print("one");
+                    break;
+                case 2:
+                System.out.print("two");
+                break;
+                case 3:
+                System.out.print("three");
+                break;
+                case 4:
+                System.out.print("four");
+                break;
+                case 5:
+                System.out.print("five");
+                break;
+                case 6:
+                System.out.print("six");
+                break;
+                case 7:
+                System.out.print("seven");
+                break;
+                case 8:
+                System.out.print("eight");
+                break;
+                case 9:
+                System.out.print("nine");
+                break;
+                default:
+                System.out.println("digit error");
+                break;
+            }
+        
+        
+    }
+}
