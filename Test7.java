9326dbc8789c4460cd242dd3e081f8ade2ba9feb
SidNeilK <siddharthneilk@gmail.com>
2024-12-25 18:09:35 -0600

    basic operators

---
+++ Test7.java
@@ -0,0 +1,7 @@
+public class Test7 {
+    public static void main(String[] args) {
+        int i = 5, j = 6, k = 7, n = 3;
+        System.out.println(i + j * k - k % n);
+        System.out.println(i / n);    
+    }
+}
