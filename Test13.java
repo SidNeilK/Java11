8cf2678912cb2816a2b8a52ee8afdd3bacf2cf1b
SidNeilK <siddharthneilk@gmail.com>
2024-12-25 18:14:31 -0600

    letter values

---
+++ Test13.java
@@ -0,0 +1,10 @@
+public class Test13 {
+    public static void main(String[] args) {
+        char letter = 'A';
+        while (letter <= 'Z') {
+        System.out.println(letter + " " + (int) letter);
+        letter++;
+}
+
+    }
+}
